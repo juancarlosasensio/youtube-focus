e7cecc56ad711056988af0871fdbf05d70a38a8a
Juan Carlos Asensio <jasensio@ufm.edu>
2023-01-11 08:17:10 -0600

    Hit YouTube search w/secret api key

--- pages/api/yt-search/[query].js
+++ pages/api/yt-search/[query].js
@@ -3,13 +3,14 @@
 export default async function handler(req, res) {
   const { query } = req.query
   console.log("You've hit /api/yt-search/[query] with: ", query)
-  try { 
-    const URL = `https://hn.algolia.com/api/v1/search?query=${query}`;
-    const response = await fetch(URL);
-    const data = await response.json();
-    const articles = data.hits;
 
-    res.status(200).json(articles);
+  const YT_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q=${query}&type=video&videoEmbeddable=true&key=${process.env.YOUTUBE_KEY}`
+
+  try { 
+    const response = await fetch(YT_URL);
+    const data = await response.json();
+
+    res.status(200).json(data);
 
   } catch (err) {
     let message = err.message;
