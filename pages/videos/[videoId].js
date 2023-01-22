ab7ae9cfa064792b662ae7cef67f7fc3c588ead3
Juan Carlos Asensio <jasensio@ufm.edu>
2023-01-22 15:22:14 -0600

    Redirect unembeddable vids

--- pages/videos/[videoId].js
+++ pages/videos/[videoId].js
@@ -1,5 +1,20 @@
 import { useRouter } from 'next/router'
 
+export async function getServerSideProps(context) {
+  const { videoId } = context.query;
+  const response = await fetch(`https://www.youtube.com/embed/${videoId}`);
+  const data = await response.text()
+
+  // Identify un-embeddable vids: https://stackoverflow.com/questions/71970621/how-to-identify-the-youtube-videos-that-cant-embed-on-file
+  if (!data) {
+    return { redirect: { destination: '/', permanent: false }}
+  } else if (data.includes('UNPLAYABLE')) {
+     return { redirect: { destination: `https://www.youtube.com/watch?v=${videoId}`, permanent: false }} 
+  } else {
+    return { props: { data } }
+  }
+}
+
 const Video = () => {
   const router = useRouter()
   const { videoId, videoTitle } = router.query
