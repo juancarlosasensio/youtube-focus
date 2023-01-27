f038f7ff551920754f2e1807ddce2056d75c7b54
Juan Carlos Asensio <jasensio@ufm.edu>
2023-01-27 17:50:52 -0600

    Use aspect-ratio for responsive vids

--- pages/videos/[videoId].js
+++ pages/videos/[videoId].js
@@ -22,9 +22,8 @@ const Video = () => {
   return (
     <div>
       <iframe
-        style={{ border: '0px' }} 
-        width="560" 
-        height="315" 
+        className='yt-video'
+        style={{ border: '0px' }}  
         src={`https://www.youtube.com/embed/${decodeURIComponent(videoId)}`}
         title={decodeURIComponent(videoTitle)}  
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
