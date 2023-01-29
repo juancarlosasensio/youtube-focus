9ece4a60260368bd920e94200652f9a739d8bc43
Juan Carlos Asensio <jasensio@ufm.edu>
2023-01-28 18:28:57 -0600

    Test if static iframe works

--- pages/videos/[videoId].js
+++ pages/videos/[videoId].js
@@ -24,6 +24,7 @@ const SingleVideoEmbedPage = () => {
   return (
     <div className="App">
       <YoutubeVideo />
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/UwRZxYw7XMA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   )
 }
