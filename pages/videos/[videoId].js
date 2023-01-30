59538e840528dd0be5e5803113a5ca250241b721
Juan Carlos Asensio <jasensio@ufm.edu>
2023-01-29 18:26:42 -0600

    Try yet another lib

--- pages/videos/[videoId].js
+++ pages/videos/[videoId].js
@@ -1,4 +1,5 @@
-import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 
 export async function getServerSideProps(context) {
   const { videoId } = context.query;
@@ -15,16 +16,20 @@ export async function getServerSideProps(context) {
   }
 }
 
-const YoutubeVideo = dynamic(
-  () => import('../../components/YoutubeVideo.jsx'),
-  { loading: () => <p>Loading...</p> }
-)
-
 const SingleVideoEmbedPage = () => {
+  const router = useRouter();
+  const { videoId, videoTitle } = router.query;
   return (
-    <div className="App">
-      <p>Hello</p>
-      <YoutubeVideo />
+    <div>
+      <LiteYouTubeEmbed
+        id={videoId} // Default none, id of the video or playlist
+        adNetwork={true} // Default true, to preconnect or not to doubleclick addresses called by YouTube iframe (the adnetwork from Google)
+        params="" // any params you want to pass to the URL, assume we already had '&' and pass your parameters string
+        playlist={false} // Use  true when your ID be from a playlist
+        poster="hqdefault" // Defines the image size to call on first render as poster image. Possible values are "default","mqdefault",  "hqdefault", "sddefault" and "maxresdefault". Default value for this prop is "hqdefault". Please be aware that "sddefault" and "maxresdefault", high resolution images are not always avaialble for every video. See: https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
+        title={videoTitle} // a11y, always provide a title for iFrames: https://dequeuniversity.com/tips/provide-iframe-titles Help the web be accessible ;)
+        noCookie={true} //Default false, connect to YouTube via the Privacy-Enhanced Mode using https://www.youtube-nocookie.com
+      />
     </div>
   )
 }
